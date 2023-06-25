eef71b76a074b0caef5b8f05a26aad8f537db7a0
JhonnSantiagoBernalJuradoCampus <jhonbernalcampus@gmail.com>
2023-06-25 16:37:22 -0500

    feat: :rocket: Hola mundo con Dart
    Concatenacion e imprimir en pantalla con dart

---
+++ 01_hello_world.dart
@@ -0,0 +1,13 @@
+void main(){
+
+  
+//   var myName = 'Santiago';
+//   late final myName;
+//   String myName = 'Santiago';
+  const myName = 'Santiago';
+  
+  print('hola $myName');
+  print('hola ${ myName.toUpperCase() }');
+  print('hola ${1+1}')
+  
+}

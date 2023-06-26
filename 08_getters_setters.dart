bbf4b49d12b2768d3ba61a614015dbabf1352cf5
JhonnSantiagoBernalJuradoCampus <jhonbernalcampus@gmail.com>
2023-06-26 11:01:01 -0500

    feat: :sparkles: Assert
    Sirve para que si una condicion no se cumple ejecute un error, como segundo parametro se pasa el error

--- 08_getters_setters.dart
+++ 08_getters_setters.dart
@@ -1,5 +1,5 @@
 void main(){
-  final mySquare = Square( side: 10);
+  final mySquare = Square( side: -10);
   
   mySquare.side = 5;
   
@@ -13,11 +13,11 @@ class Square {
   double _side; // side * side
   
   Square({required double side})
-    : _side = side;
+    : assert(side >= 0, 'Side must be >= 0'),
+      _side = side;
   
-  double get area {
-    return _side * _side;
-  }
+  double get area => _side * _side;
+  
   
   set side( double value) {
     print('Setting new value $value');

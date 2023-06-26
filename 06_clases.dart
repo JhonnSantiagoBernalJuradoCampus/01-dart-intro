664567f7b90909e1a5c521725606086c6fe09af9
JhonnSantiagoBernalJuradoCampus <jhonbernalcampus@gmail.com>
2023-06-26 09:14:21 -0500

    feat: :sparkles: @override
    sobre escribir funciones como toString();

--- 06_clases.dart
+++ 06_clases.dart
@@ -1,19 +1,28 @@
-void main(){
-  final Hero wolverine = Hero('Logan', 'Regeneración');
-  
-  print(wolverine);
-  print(wolverine.name);
-  print(wolverine.power);
-}
+  void main(){
+    final Hero wolverine = Hero(name: 'Logan');
 
-class Hero {
-  
-  String name;
-  String power;
-  
-  Hero(this.name, this.power);
-//  Hero( String pName, String pPower )
-//    : name = pName,
-//      power = pPower;
-  
-}
+    print(wolverine.toString());
+    print(wolverine.name);
+    print(wolverine.power);
+  }
+
+  class Hero {
+
+    String name;
+    String power;
+
+    Hero({
+      required this.name, 
+      this.power = 'Sin poder'
+    });
+  //  Hero( String pName, String pPower )
+  //    : name = pName,
+  //      power = pPower;
+
+    @override
+    String toString(){
+      return '$name - $power';
+    }
+    
+    
+  }
